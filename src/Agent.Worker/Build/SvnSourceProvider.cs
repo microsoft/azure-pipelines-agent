6e8e2398c4a90680e35f6638b410d13d7b7d1115
Alex Rukhlin <arukhlin@microsoft.com>
2016-04-28 14:28:50 -0400

    Adding SVN support

--- src/Agent.Worker/Build/SvnSourceProvider.cs
+++ src/Agent.Worker/Build/SvnSourceProvider.cs
@@ -4,6 +4,10 @@ using System;
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Services.Agent.Util;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Microsoft.VisualStudio.Services.Agent.Worker.Build
 {
@@ -11,14 +15,49 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Build
     {
         public override string RepositoryType => WellKnownRepositoryTypes.Svn;
 
-        public Task GetSourceAsync(IExecutionContext executionContext, ServiceEndpoint endpoint, CancellationToken cancellationToken)
+        public async Task GetSourceAsync(IExecutionContext executionContext, ServiceEndpoint endpoint, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            executionContext.Debug("Entering SvnSourceProvider.GetSourceAsync");
+
+            // Validate args.
+            ArgUtil.NotNull(executionContext, nameof(executionContext));
+            ArgUtil.NotNull(endpoint, nameof(endpoint));
+
+            ISvnCommandManager svn = HostContext.CreateService<ISvnCommandManager>();
+            svn.Init(executionContext, endpoint, cancellationToken);
+
+            // Determine the sources directory.
+            string sourcesDirectory = executionContext.Variables.Build_SourcesDirectory;
+            ArgUtil.NotNullOrEmpty(sourcesDirectory, nameof(sourcesDirectory));
+
+            string sourceBranch = executionContext.Variables.Build_SourceBranch;
+            string revision = executionContext.Variables.Build_SourceVersion;
+
+            bool clean = endpoint.Data.ContainsKey(WellKnownEndpointData.Clean) &&
+                StringUtil.ConvertToBoolean(endpoint.Data[WellKnownEndpointData.Clean], defaultValue: false);
+
+            // Get the definition mappings.
+            List<SvnMappingDetails> allMappings = JsonConvert.DeserializeObject<SvnWorkspace>(endpoint.Data[WellKnownEndpointData.SvnWorkspaceMapping]).Mappings;
+
+            Dictionary<string, SvnMappingDetails> normalizedMappings = svn.NormalizeMappings(allMappings);
+            string normalizedBranch = svn.NormalizeRelativePath(sourceBranch, '/', '\\');
+
+            executionContext.Output(StringUtil.Loc("SvnSyncingRepo", endpoint.Name));
+
+            string effectiveRevision = await svn.UpdateWorkspace(
+                sourcesDirectory,
+                normalizedMappings,
+                clean,
+                normalizedBranch,
+                revision);
+
+            executionContext.Output(StringUtil.Loc("SvnBranchCheckedOut", normalizedBranch, endpoint.Name, effectiveRevision));
+            executionContext.Debug("Leaving SvnSourceProvider.GetSourceAsync");
         }
 
         public Task PostJobCleanupAsync(IExecutionContext executionContext, ServiceEndpoint endpoint)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
