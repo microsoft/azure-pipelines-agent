725e0e713eb45c513800c18d67708f95fb4a6f42
Jegan Lingam <jegan.lingam@microsoft.com>
2016-10-14 11:48:37 -0700

    job prepare validator

--- src/Agent.Worker/Release/ReleaseJobExtension.cs
+++ src/Agent.Worker/Release/ReleaseJobExtension.cs
@@ -113,6 +113,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Release
             bool skipArtifactsDownload;
 
             IExecutionContext executionContext = PrepareStep.ExecutionContext;
+            new JobPrepareValidator().Validate(HostType, executionContext.Variables.GetInt(Constants.Variables.Release.ReleaseId) ?? 0);
 
             try
             {
