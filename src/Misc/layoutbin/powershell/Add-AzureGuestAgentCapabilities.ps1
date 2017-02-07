426639a442d831f2659b767cf4544cb1031bf468
Krishna Aditya <krishadi@microsoft.com>
2017-02-07 17:41:02 +0530

    adding azureGuestAgent path as value for the capability to be consistent between linux and windows

--- src/Misc/layoutbin/powershell/Add-AzureGuestAgentCapabilities.ps1
+++ src/Misc/layoutbin/powershell/Add-AzureGuestAgentCapabilities.ps1
@@ -1,12 +1,24 @@
 [CmdletBinding()]
 param()
 
+$guestAgentPath = $null
+
 try {
-    $proc = Get-Process WindowsAzureGuestAgent
-    if ($proc) {
-        Write-Capability -Name 'AzureGuestAgent' -Value 'Yes'
+    $proc = Get-Process 'WindowsAzureGuestAgent'
+
+    if($proc) {
+        if (($proc -is [System.Array]) -and ($proc.Count -gt 0)) {
+            $guestAgentPath = $proc[0].Path
+        }
+        else {
+            $guestAgentPath = $proc.Path
+        }
     }
 }
 catch {
+}
+
+if($guestAgentPath) {
+    Write-Capability -Name 'AzureGuestAgent' -Value $guestAgentPath
 }
 
