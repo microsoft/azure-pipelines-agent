19ce380642f892474ab97ac6dbf3a6377ac165ea
Krishna Aditya <krishadi@microsoft.com>
2017-02-13 18:53:25 +0530

    refactoring the powershell script

--- src/Misc/layoutbin/powershell/Add-AzureGuestAgentCapabilities.ps1
+++ src/Misc/layoutbin/powershell/Add-AzureGuestAgentCapabilities.ps1
@@ -1,24 +1,4 @@
 [CmdletBinding()]
 param()
 
-$guestAgentPath = $null
-
-try {
-    $proc = Get-Process 'WindowsAzureGuestAgent'
-
-    if($proc) {
-        if (($proc -is [System.Array]) -and ($proc.Count -gt 0)) {
-            $guestAgentPath = $proc[0].Path
-        }
-        else {
-            $guestAgentPath = $proc.Path
-        }
-    }
-}
-catch {
-}
-
-if($guestAgentPath) {
-    Write-Capability -Name 'AzureGuestAgent' -Value $guestAgentPath
-}
-
+@(Get-Process -Name 'WindowsAzureGuestAgent' -ErrorAction Ignore) | Select-Object -First 1 | ForEach-Object { Write-Capability -Name 'AzureGuestAgent' -Value $_.Path }
