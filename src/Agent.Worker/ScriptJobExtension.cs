3128d8ee9c41b42a348b60629660de683c6f2692
kirillp <kirillp@microsoft.com>
2017-02-22 10:15:39 -0800

    Added comments for script job extensions

--- src/Agent.Worker/ScriptJobExtension.cs
+++ src/Agent.Worker/ScriptJobExtension.cs
@@ -7,6 +7,7 @@ using Microsoft.VisualStudio.Services.Agent.Worker.Handlers;
 
 namespace Microsoft.VisualStudio.Services.Agent.Worker
 {
+    // This class is for internal testing and is not publicly supported. This will be removed from the agent at a later time.
     public class ScriptJobExtension : AgentService, IJobExtension
     {
         public Type ExtensionType => typeof(IJobExtension);
