475bcd63a44ad0c6f62cbf5ec721850c2795fe99
Nick Muller <muller_nicky@hotmail.com>
2017-08-30 22:07:44 +0200

    Added Meteor PowerShell and Nix capability.

--- src/Agent.Listener/Capabilities/NixCapabilitiesProvider.cs
+++ src/Agent.Listener/Capabilities/NixCapabilitiesProvider.cs
@@ -50,6 +50,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener.Capabilities
             builder.Check(name: "JDK", fileName: "javac");
             builder.Check(name: "make");
             builder.Check(name: "maven", fileName: "mvn");
+            builder.Check(name: "meteor");
             builder.Check(name: "MSBuild", fileName: "xbuild");
             builder.Check(name: "node.js", fileName: "node");
             builder.Check(name: "node.js", fileName: "nodejs");
