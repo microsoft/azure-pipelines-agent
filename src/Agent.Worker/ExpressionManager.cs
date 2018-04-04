JacobHenner/vsts-agent

log expression info trace for skipped step.

src/Agent.Worker/ExpressionManager.cs
@@ -3,55 +3,65 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using Microsoft.VisualStudio.Services.Agent.Util;
-using Expressions = Microsoft.TeamFoundation.DistributedTask.Orchestration.Server.Expressions;
+using Microsoft.TeamFoundation.DistributedTask.Expressions;
+using System.Text;
 
 namespace Microsoft.VisualStudio.Services.Agent.Worker
 {
     [ServiceLocator(Default = typeof(ExpressionManager))]
     public interface IExpressionManager : IAgentService
     {
-        Expressions.INode Parse(IExecutionContext context, string condition);
-        bool Evaluate(IExecutionContext context, Expressions.INode tree, bool hostTracingOnly = false);
+        IExpressionNode Parse(IExecutionContext context, string condition);
+        ConditionResult Evaluate(IExecutionContext context, IExpressionNode tree, bool hostTracingOnly = false);
     }
 
     public sealed class ExpressionManager : AgentService, IExpressionManager
     {
-        public static Expressions.INode Always = new AlwaysNode();
-        public static Expressions.INode Succeeded = new SucceededNode();
-        public static Expressions.INode SucceededOrFailed = new SucceededOrFailedNode();
+        public static IExpressionNode Always = new AlwaysNode();
+        public static IExpressionNode Succeeded = new SucceededNode();
+        public static IExpressionNode SucceededOrFailed = new SucceededOrFailedNode();
 
-        public Expressions.INode Parse(IExecutionContext executionContext, string condition)
+        public IExpressionNode Parse(IExecutionContext executionContext, string condition)
         {
             ArgUtil.NotNull(executionContext, nameof(executionContext));
             var expressionTrace = new TraceWriter(Trace, executionContext);
-            var parser = new Expressions.Parser();
-            var namedValues = new Expressions.INamedValueInfo[]
+            var parser = new ExpressionParser();
+            var namedValues = new INamedValueInfo[]
             {
-                new Expressions.NamedValueInfo<VariablesNode>(name: Constants.Expressions.Variables),
+                new NamedValueInfo<VariablesNode>(name: Constants.Expressions.Variables),
             };
-            var functions = new Expressions.IFunctionInfo[]
+            var functions = new IFunctionInfo[]
             {
-                new Expressions.FunctionInfo<AlwaysNode>(name: Constants.Expressions.Always, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<CanceledNode>(name: Constants.Expressions.Canceled, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<FailedNode>(name: Constants.Expressions.Failed, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<SucceededNode>(name: Constants.Expressions.Succeeded, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<SucceededOrFailedNode>(name: Constants.Expressions.SucceededOrFailed, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<AlwaysNode>(name: Constants.Expressions.Always, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<CanceledNode>(name: Constants.Expressions.Canceled, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<FailedNode>(name: Constants.Expressions.Failed, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<SucceededNode>(name: Constants.Expressions.Succeeded, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<SucceededOrFailedNode>(name: Constants.Expressions.SucceededOrFailed, minParameters: 0, maxParameters: 0),
             };
             return parser.CreateTree(condition, expressionTrace, namedValues, functions) ?? new SucceededNode();
         }
 
-        public bool Evaluate(IExecutionContext executionContext, Expressions.INode tree, bool hostTracingOnly = false)
+        public ConditionResult Evaluate(IExecutionContext executionContext, IExpressionNode tree, bool hostTracingOnly = false)
         {
             ArgUtil.NotNull(executionContext, nameof(executionContext));
             ArgUtil.NotNull(tree, nameof(tree));
+
+            ConditionResult result = new ConditionResult();
             var expressionTrace = new TraceWriter(Trace, hostTracingOnly ? null : executionContext);
-            return tree.EvaluateBoolean(trace: expressionTrace, state: executionContext);
+
+            result.Value = tree.EvaluateBoolean(trace: expressionTrace, secretMasker: null, state: executionContext);
+            result.Trace = expressionTrace.ResultCode;
+
+            return result;
         }
 
-        private sealed class TraceWriter : Expressions.ITraceWriter
+        private sealed class TraceWriter : ITraceWriter
         {
             private readonly IExecutionContext _executionContext;
             private readonly Tracing _trace;
+            private readonly StringBuilder _resultCodeBuilder = new StringBuilder();
+
+            public string ResultCode => _resultCodeBuilder.ToString();
 
             public TraceWriter(Tracing trace, IExecutionContext executionContext)
             {
@@ -64,6 +74,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             {
                 _trace.Info(message);
                 _executionContext?.Debug(message);
+                _resultCodeBuilder.AppendLine(message);
             }
 
             public void Verbose(string message)
@@ -73,21 +84,17 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class AlwaysNode : Expressions.FunctionNode
+        private sealed class AlwaysNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Always;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected override Object EvaluateCore(EvaluationContext context)
             {
                 return true;
             }
         }
 
-        private sealed class CanceledNode : Expressions.FunctionNode
+        private sealed class CanceledNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Canceled;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -96,11 +103,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class FailedNode : Expressions.FunctionNode
+        private sealed class FailedNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Failed;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -109,11 +114,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class SucceededNode : Expressions.FunctionNode
+        private sealed class SucceededNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Succeeded;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -123,11 +126,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class SucceededOrFailedNode : Expressions.FunctionNode
+        private sealed class SucceededOrFailedNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.SucceededOrFailed;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -138,11 +139,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class VariablesNode : Expressions.NamedValueNode
+        private sealed class VariablesNode : NamedValueNode
         {
-            public sealed override string Name => Constants.Expressions.Variables;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var jobContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(jobContext, nameof(jobContext));
@@ -190,5 +189,17 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             // IEnumerable members
             IEnumerator IEnumerable.GetEnumerator() => throw new NotSupportedException();
         }
+    }
+
+    public class ConditionResult
+    {
+        public ConditionResult(bool value = false, string trace = null)
+        {
+            this.Value = value;
+            this.Trace = trace;
+        }
+
+        public bool Value { get; set; }
+        public string Trace { get; set; }
     }
 }

microsoft/azure-pipelines-agent

src/Agent.Worker/ExpressionManager.cs
@@ -3,55 +3,65 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using Microsoft.VisualStudio.Services.Agent.Util;
-using Expressions = Microsoft.TeamFoundation.DistributedTask.Orchestration.Server.Expressions;
+using Microsoft.TeamFoundation.DistributedTask.Expressions;
+using System.Text;
 
 namespace Microsoft.VisualStudio.Services.Agent.Worker
 {
     [ServiceLocator(Default = typeof(ExpressionManager))]
     public interface IExpressionManager : IAgentService
     {
-        Expressions.INode Parse(IExecutionContext context, string condition);
-        bool Evaluate(IExecutionContext context, Expressions.INode tree, bool hostTracingOnly = false);
+        IExpressionNode Parse(IExecutionContext context, string condition);
+        ConditionResult Evaluate(IExecutionContext context, IExpressionNode tree, bool hostTracingOnly = false);
     }
 
     public sealed class ExpressionManager : AgentService, IExpressionManager
     {
-        public static Expressions.INode Always = new AlwaysNode();
-        public static Expressions.INode Succeeded = new SucceededNode();
-        public static Expressions.INode SucceededOrFailed = new SucceededOrFailedNode();
+        public static IExpressionNode Always = new AlwaysNode();
+        public static IExpressionNode Succeeded = new SucceededNode();
+        public static IExpressionNode SucceededOrFailed = new SucceededOrFailedNode();
 
-        public Expressions.INode Parse(IExecutionContext executionContext, string condition)
+        public IExpressionNode Parse(IExecutionContext executionContext, string condition)
         {
             ArgUtil.NotNull(executionContext, nameof(executionContext));
             var expressionTrace = new TraceWriter(Trace, executionContext);
-            var parser = new Expressions.Parser();
-            var namedValues = new Expressions.INamedValueInfo[]
+            var parser = new ExpressionParser();
+            var namedValues = new INamedValueInfo[]
             {
-                new Expressions.NamedValueInfo<VariablesNode>(name: Constants.Expressions.Variables),
+                new NamedValueInfo<VariablesNode>(name: Constants.Expressions.Variables),
             };
-            var functions = new Expressions.IFunctionInfo[]
+            var functions = new IFunctionInfo[]
             {
-                new Expressions.FunctionInfo<AlwaysNode>(name: Constants.Expressions.Always, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<CanceledNode>(name: Constants.Expressions.Canceled, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<FailedNode>(name: Constants.Expressions.Failed, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<SucceededNode>(name: Constants.Expressions.Succeeded, minParameters: 0, maxParameters: 0),
-                new Expressions.FunctionInfo<SucceededOrFailedNode>(name: Constants.Expressions.SucceededOrFailed, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<AlwaysNode>(name: Constants.Expressions.Always, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<CanceledNode>(name: Constants.Expressions.Canceled, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<FailedNode>(name: Constants.Expressions.Failed, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<SucceededNode>(name: Constants.Expressions.Succeeded, minParameters: 0, maxParameters: 0),
+                new FunctionInfo<SucceededOrFailedNode>(name: Constants.Expressions.SucceededOrFailed, minParameters: 0, maxParameters: 0),
             };
             return parser.CreateTree(condition, expressionTrace, namedValues, functions) ?? new SucceededNode();
         }
 
-        public bool Evaluate(IExecutionContext executionContext, Expressions.INode tree, bool hostTracingOnly = false)
+        public ConditionResult Evaluate(IExecutionContext executionContext, IExpressionNode tree, bool hostTracingOnly = false)
         {
             ArgUtil.NotNull(executionContext, nameof(executionContext));
             ArgUtil.NotNull(tree, nameof(tree));
+
+            ConditionResult result = new ConditionResult();
             var expressionTrace = new TraceWriter(Trace, hostTracingOnly ? null : executionContext);
-            return tree.EvaluateBoolean(trace: expressionTrace, state: executionContext);
+
+            result.Value = tree.Evaluate<bool>(trace: expressionTrace, secretMasker: HostContext.SecretMasker, state: executionContext);
+            result.Trace = expressionTrace.Trace;
+
+            return result;
         }
 
-        private sealed class TraceWriter : Expressions.ITraceWriter
+        private sealed class TraceWriter : ITraceWriter
         {
             private readonly IExecutionContext _executionContext;
             private readonly Tracing _trace;
+            private readonly StringBuilder _traceBuilder = new StringBuilder();
+
+            public string Trace => _traceBuilder.ToString();
 
             public TraceWriter(Tracing trace, IExecutionContext executionContext)
             {
@@ -64,6 +74,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             {
                 _trace.Info(message);
                 _executionContext?.Debug(message);
+                _traceBuilder.AppendLine(message);
             }
 
             public void Verbose(string message)
@@ -73,21 +84,17 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class AlwaysNode : Expressions.FunctionNode
+        private sealed class AlwaysNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Always;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected override Object EvaluateCore(EvaluationContext context)
             {
                 return true;
             }
         }
 
-        private sealed class CanceledNode : Expressions.FunctionNode
+        private sealed class CanceledNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Canceled;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -96,11 +103,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class FailedNode : Expressions.FunctionNode
+        private sealed class FailedNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Failed;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -109,11 +114,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class SucceededNode : Expressions.FunctionNode
+        private sealed class SucceededNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.Succeeded;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -123,11 +126,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class SucceededOrFailedNode : Expressions.FunctionNode
+        private sealed class SucceededOrFailedNode : FunctionNode
         {
-            public sealed override string Name => Constants.Expressions.SucceededOrFailed;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var executionContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(executionContext, nameof(executionContext));
@@ -138,11 +139,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
             }
         }
 
-        private sealed class VariablesNode : Expressions.NamedValueNode
+        private sealed class VariablesNode : NamedValueNode
         {
-            public sealed override string Name => Constants.Expressions.Variables;
-
-            protected sealed override object EvaluateCore(Expressions.EvaluationContext evaluationContext)
+            protected sealed override object EvaluateCore(EvaluationContext evaluationContext)
             {
                 var jobContext = evaluationContext.State as IExecutionContext;
                 ArgUtil.NotNull(jobContext, nameof(jobContext));
@@ -189,6 +188,23 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
 
             // IEnumerable members
             IEnumerator IEnumerable.GetEnumerator() => throw new NotSupportedException();
+        }
+    }
+
+    public class ConditionResult
+    {
+        public ConditionResult(bool value = false, string trace = null)
+        {
+            this.Value = value;
+            this.Trace = trace;
+        }
+
+        public bool Value { get; set; }
+        public string Trace { get; set; }
+
+        public static implicit operator ConditionResult(bool value)
+        {
+            return new ConditionResult(value);
         }
     }
 }