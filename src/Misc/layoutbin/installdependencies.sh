1677cf6ec5822f12089e2fc7949987f1066bdeb9
Greg Myers <neonmd@hotmail.co.uk>
2018-11-28 15:50:48 +0000

    Update installdependencies.sh

--- src/Misc/layoutbin/installdependencies.sh
+++ src/Misc/layoutbin/installdependencies.sh
@@ -36,7 +36,17 @@ then
         command -v apt
         if [ $? -eq 0 ]
         then
-            apt update && apt install -y liblttng-ust0 libcurl4 libkrb5-3 zlib1g
+            apt update && apt install -y liblttng-ust0 libkrb5-3 zlib1g
+            if [ $? -ne 0 ]
+            then
+                echo "'apt' failed with exit code '$?'"
+                print_errormessage
+                exit 1
+            fi
+            
+            # ubuntu 18 uses libcurl4
+            # ubuntu 14, 16 and other linux use libcurl3
+            apt install -y libcurl4 || apt install -y libcurl3
             if [ $? -ne 0 ]
             then
                 echo "'apt' failed with exit code '$?'"
