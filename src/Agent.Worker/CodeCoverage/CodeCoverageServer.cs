b29d9b9cad9be91a086222c5f55b6269712ec0ec
jagarg <jagarg@microsoft.com>
2019-01-23 11:40:42 +0530

    Redirecting CC data to TFS/TCM depending on FF

--- src/Agent.Worker/CodeCoverage/CodeCoverageServer.cs
+++ src/Agent.Worker/CodeCoverage/CodeCoverageServer.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Microsoft.VisualStudio.Services.Agent.Util;
+using Microsoft.VisualStudio.Services.FeatureAvailability.WebApi;
+using Microsoft.VisualStudio.Services.TestResults.WebApi;
 using Microsoft.VisualStudio.Services.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -13,14 +17,13 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.CodeCoverage
         /// <summary>
         /// Publish code coverage summary
         /// </summary>
-        Task PublishCoverageSummaryAsync(VssConnection connection, string project, int buildId, IEnumerable<CodeCoverageStatistics> coverageData, CancellationToken cancellationToken);
+        Task PublishCoverageSummaryAsync(IAsyncCommandContext context, VssConnection connection, string project, int buildId, IEnumerable<CodeCoverageStatistics> coverageData, CancellationToken cancellationToken);
     }
 
     internal sealed class CodeCoverageServer : AgentService, ICodeCoverageServer
     {
-        public async Task PublishCoverageSummaryAsync(VssConnection connection, string project, int buildId, IEnumerable<CodeCoverageStatistics> coverageData, CancellationToken cancellationToken)
+        public async Task PublishCoverageSummaryAsync(IAsyncCommandContext context, VssConnection connection, string project, int buildId, IEnumerable<CodeCoverageStatistics> coverageData, CancellationToken cancellationToken)
         {
-            var testHttpClient = connection.GetClient<TestManagementHttpClient>();
             // <todo: Bug 402783> We are currently passing BuildFlavor and BuildPlatform = "" There value are required be passed to command
             CodeCoverageData data = new CodeCoverageData()
             {
@@ -29,7 +32,35 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.CodeCoverage
                 CoverageStats = coverageData.ToList()
             };
 
-            await testHttpClient.UpdateCodeCoverageSummaryAsync(data, project, buildId, cancellationToken: cancellationToken);
+            FeatureAvailabilityHttpClient featureAvailabilityHttpClient = connection.GetClient<FeatureAvailabilityHttpClient>();
+            if (GetFeatureFlagState(featureAvailabilityHttpClient, CodeCoverageConstants.EnablePublishToTcmServiceDirectlyFromTaskFF, context))
+            {
+                TestResultsHttpClient tcmClient = connection.GetClient<TestResultsHttpClient>();
+                await tcmClient.UpdateCodeCoverageSummaryAsync(data, project, buildId, cancellationToken: cancellationToken);
+            }
+            else
+            {
+                TestManagementHttpClient tfsClient = connection.GetClient<TestManagementHttpClient>();
+                await tfsClient.UpdateCodeCoverageSummaryAsync(data, project, buildId, cancellationToken: cancellationToken);
+            }
+        }
+
+        private bool GetFeatureFlagState(FeatureAvailabilityHttpClient featureAvailabilityHttpClient, string FFName, IAsyncCommandContext context)
+        {
+            try
+            {
+                var featureFlag = featureAvailabilityHttpClient?.GetFeatureFlagByNameAsync(FFName).Result;
+                if (featureFlag != null && featureFlag.EffectiveState.Equals("On", StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                context.Debug(StringUtil.Format("Failed to get FF {0} Value. By default, publishing data to TCM.", FFName));
+                return true;
+            }
+            return false;
         }
     }
 }
