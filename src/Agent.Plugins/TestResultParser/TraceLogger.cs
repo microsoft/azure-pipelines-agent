9fd0edc68822e1f43db8915c7175e69d8c80b358
🏆Nitin Gurram🏆 <nigurr@microsoft.com>
2019-01-29 11:51:58 +0530

    Fixed trace

--- src/Agent.Plugins/TestResultParser/TraceLogger.cs
+++ src/Agent.Plugins/TestResultParser/TraceLogger.cs
@@ -15,19 +15,19 @@ namespace Agent.Plugins.TestResultParser.Plugin
         /// <inheritdoc />
         void ITraceLogger.Warning(string text)
         {
-            _context.Output($"##vso[task.logissue type=warning;]{text}");
+            _context.Output($"Warning: {text}");
         }
 
         /// <inheritdoc />
         void ITraceLogger.Error(string error)
         {
-            _context.Output($"##vso[task.logissue type=error;]{error}");
+            _context.Output($"Error: {error}");
         }
 
         /// <inheritdoc />
         void ITraceLogger.Verbose(string text)
         {
-            _context.Output($"##vso[task.debug]{text}");
+            _context.Output($"Debug: {text}");
         }
 
         /// <inheritdoc />
