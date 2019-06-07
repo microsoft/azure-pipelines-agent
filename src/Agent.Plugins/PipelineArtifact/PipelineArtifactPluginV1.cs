0b89eed6429efc9722b4765294f602a7a3a17114
jajan <jahsu@microsoft.com>
2019-06-07 10:47:04 -0700

    remove changes in V1

--- src/Agent.Plugins/PipelineArtifact/PipelineArtifactPluginV1.cs
+++ src/Agent.Plugins/PipelineArtifact/PipelineArtifactPluginV1.cs
@@ -148,12 +148,6 @@ namespace Agent.Plugins.PipelineArtifact
             string tags = context.GetInput(ArtifactEventProperties.Tags, required: false);
             string userSpecifiedpipelineId = context.GetInput(PipelineId, required: false);
 
-            if (!Directory.Exists(targetPath) && !File.Exists(targetPath))
-            {
-                // if local path is neither file nor folder
-                throw new FileNotFoundException(StringUtil.Loc("PathNotExist", targetPath));
-            }   
-
             string[] minimatchPatterns = itemPattern.Split(
                 new[] { "\n" },
                 StringSplitOptions.RemoveEmptyEntries
