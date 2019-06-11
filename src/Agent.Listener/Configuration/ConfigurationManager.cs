9a627669784e44614f8a8d94c150e6e7eda6bd89
asaygo <asaygo@users.noreply.github.com>
2019-06-11 10:57:26 -0700

    include System.Security.AccessControl library

--- src/Agent.Listener/Configuration/ConfigurationManager.cs
+++ src/Agent.Listener/Configuration/ConfigurationManager.cs
@@ -14,6 +14,7 @@ using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 
 namespace Microsoft.VisualStudio.Services.Agent.Listener.Configuration
 {
