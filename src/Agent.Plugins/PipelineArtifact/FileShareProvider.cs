aaba17f47245a8ec03ecff4f442887f002a51a77
jajan <jahsu@microsoft.com>
2019-09-10 13:48:18 -0700

    formatting

--- src/Agent.Plugins/PipelineArtifact/FileShareProvider.cs
+++ src/Agent.Plugins/PipelineArtifact/FileShareProvider.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
+using Agent.Plugins.PipelineArtifact.Telemetry;
 using Agent.Sdk;
 using Microsoft.TeamFoundation.Build.WebApi;
-using Microsoft.VisualStudio.Services.BlobStore.Common;
-using Microsoft.VisualStudio.Services.Content.Common.Tracing;
 using Microsoft.VisualStudio.Services.Agent.Util;
-using Microsoft.VisualStudio.Services.Content.Common;
+using Microsoft.VisualStudio.Services.BlobStore.Common;
 using Microsoft.VisualStudio.Services.BlobStore.Common.Telemetry;
-using Agent.Plugins.PipelineArtifact.Telemetry;
 using Microsoft.VisualStudio.Services.BlobStore.WebApi;
-using System.Diagnostics;
+using Microsoft.VisualStudio.Services.Content.Common;
+using Microsoft.VisualStudio.Services.Content.Common.Tracing;
 using Microsoft.VisualStudio.Services.WebApi;
 
 namespace Agent.Plugins.PipelineArtifact
