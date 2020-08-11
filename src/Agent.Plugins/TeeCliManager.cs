413a54ce827cf1623eada6e4cc94b814a0a57084
RuslanRasulovAkv <66998753+RuslanRasulovAkv@users.noreply.github.com>
2020-08-11 10:21:52 +0300

    Update TeeCliManager.cs

--- src/Agent.Plugins/TeeCliManager.cs
+++ src/Agent.Plugins/TeeCliManager.cs
@@ -231,7 +231,7 @@ namespace Agent.Plugins.Repository
             await RunCommandAsync(FormatFlags.OmitCollectionUrl, "undo", "-recursive", localPath);
         }
 
-        public async Task UnshelveAsync(string shelveset, bool failOnNonZeroExitCode = false)
+        public async Task UnshelveAsync(string shelveset, bool failOnNonZeroExitCode = true)
         {
             ArgUtil.NotNullOrEmpty(shelveset, nameof(shelveset));
             await RunCommandAsync(FormatFlags.OmitCollectionUrl, false, failOnNonZeroExitCode, "unshelve", "-format:detailed", $"-workspace:{WorkspaceName}", shelveset);
