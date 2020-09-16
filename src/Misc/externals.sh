d0cd35f7d59cc0808666c6caa2c1626fcd973ab0
Alex Peck <alpeck@microsoft.com>
2020-09-16 09:58:52 -0400

    Node sec

--- src/Misc/externals.sh
+++ src/Misc/externals.sh
@@ -8,7 +8,7 @@ CONTAINER_URL=https://vstsagenttools.blob.core.windows.net/tools
 NODE_URL=https://nodejs.org/dist
 NODE_VERSION="6.17.1"
 NODE10_VERSION="10.21.0"
-NODE14_VERSION="14.10.1"
+NODE14_VERSION="14.11.0"
 MINGIT_VERSION="2.26.2"
 
 get_abs_path() {
