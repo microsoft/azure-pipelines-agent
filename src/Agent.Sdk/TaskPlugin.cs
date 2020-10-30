b8c67b9dfd65ffb6175ce1698411894865a9e27f
damccorm <damccorm@microsoft.com>
2020-10-30 13:04:48 -0400

    Fix

--- src/Agent.Sdk/TaskPlugin.cs
+++ src/Agent.Sdk/TaskPlugin.cs
@@ -355,7 +355,7 @@ namespace Agent.Sdk
             return new SystemEnvironment();
         }
 
-        private Dictionary<string, string> _commandEscapeMappings = new OrderedDictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        private Dictionary<string, string> _commandEscapeMappings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
             {
                 "%", "%25"
