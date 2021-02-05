9d05a6a25dd204411285a6c8ca9bf979269f09af
Nikita Ezzhev <v-niezz@microsoft.com>
2021-02-05 10:53:41 +0300

    Fix clean param

--- src/Agent.Sdk/Knob/AgentKnobs.cs
+++ src/Agent.Sdk/Knob/AgentKnobs.cs
@@ -190,7 +190,7 @@ namespace Agent.Sdk.Knob
             nameof(DisableAgentDowngrade),
             "Disable agent downgrades. Upgrades will still be allowed.",
             new EnvironmentKnobSource("AZP_AGENT_DOWNGRADE_DISABLED"),
-            new BuiltInDefaultKnobSource("false"));
+            new BuiltInDefaultKnobSource("true"));
 
         public static readonly Knob PermissionsCheckFailsafe = new Knob(
             nameof(PermissionsCheckFailsafe),
