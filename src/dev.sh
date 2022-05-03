8f347ec3e676b61ac91218be8f169ea4854daa8c
Tanmay Fadnavis <tafadnav@microsoft.com>
2022-05-03 14:36:34 -0700

    commenting restore and nuget auth task

--- src/dev.sh
+++ src/dev.sh
@@ -100,7 +100,7 @@ function cmd_layout ()
 {
     heading "Creating layout"
     TARGET="layout"
-    for x in */*.csproj; do dotnet restore --runtime ${RUNTIME_ID} -v d $x; done;
+    #for x in */*.csproj; do dotnet restore --runtime ${RUNTIME_ID} -v d $x; done;
     if  [[ "$ADO_ENABLE_LOGISSUE" == "true" ]]; then
         dotnet msbuild -t:${TARGET} -p:PackageRuntime="${RUNTIME_ID}" -p:PackageType="${PACKAGE_TYPE}" -p:BUILDCONFIG="${BUILD_CONFIG}" -p:AgentVersion="${AGENT_VERSION}" -p:LayoutRoot="${LAYOUT_DIR}" -p:CodeAnalysis="true" \
          | sed -e "/\: warning /s/^/${DOTNET_WARNING_PREFIX} /;" \
