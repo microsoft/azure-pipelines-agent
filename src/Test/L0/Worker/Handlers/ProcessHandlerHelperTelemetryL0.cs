32b209feb0b6083bf54900ea9770528d5d5ad63a
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-09-19 14:24:16 +0400

    Simplify PH telemetry tests

--- src/Test/L0/Worker/Handlers/ProcessHandlerHelperTelemetryL0.cs
+++ src/Test/L0/Worker/Handlers/ProcessHandlerHelperTelemetryL0.cs
@@ -12,56 +12,49 @@ namespace Test.L0.Worker.Handlers
         public void FoundPrefixesTest()
         {
             string argsLine = "% % %";
-            // we're thinking that whitespaces are also may be env variables, so here the '% %' and '%' env enterances.
-            var expectedTelemetry = new { foundPrefixes = 2 };
-
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.foundPrefixes, resultTelemetry.FoundPrefixes);
+            Assert.Equal(2, resultTelemetry.FoundPrefixes);
         }
 
         [Fact]
         public void NotClosedEnv()
         {
             string argsLine = "%1";
-            var expectedTelemetry = new { NotClosedEnvSyntaxPosition = 0 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.NotClosedEnvSyntaxPosition, resultTelemetry.NotClosedEnvSyntaxPosition);
+            Assert.Equal(0, resultTelemetry.NotClosedEnvSyntaxPosition);
         }
 
         [Fact]
         public void NotClosedEnv2()
         {
             string argsLine = "\"%\" %";
-            var expectedTelemetry = new { NotClosedEnvSyntaxPosition = 4 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.NotClosedEnvSyntaxPosition, resultTelemetry.NotClosedEnvSyntaxPosition);
+            Assert.Equal(4, resultTelemetry.NotClosedEnvSyntaxPosition);
         }
 
         [Fact]
         public void NotClosedQuotes()
         {
             string argsLine = "\" %var%";
-            var expectedTelemetry = new { quotesNotEnclosed = 1 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.quotesNotEnclosed, resultTelemetry.QuotesNotEnclosed);
+            Assert.Equal(1, resultTelemetry.QuotesNotEnclosed);
         }
 
         [Fact]
         public void NotClosedQuotes_Ignore_if_no_envVar()
         {
             string argsLine = "\" 1";
-            var expectedTelemetry = new { quotesNotEnclosed = 0 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.quotesNotEnclosed, resultTelemetry.QuotesNotEnclosed);
+            Assert.Equal(0, resultTelemetry.QuotesNotEnclosed);
         }
 
         [Fact]
@@ -69,22 +62,20 @@ namespace Test.L0.Worker.Handlers
         {
             // We're ignoring quote blocks where no any env variables
             string argsLine = "\"%VAR1%\" \"%VAR2%\" \"3\"";
-            var expectedTelemetry = new { quottedBlocks = 2 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.quottedBlocks, resultTelemetry.QuottedBlocks);
+            Assert.Equal(2, resultTelemetry.QuottedBlocks);
         }
 
         [Fact]
         public void CountsVariablesStartFromEscSymbol()
         {
             string argsLine = "%^VAR1% \"%^VAR2%\" %^VAR3%";
-            var expectedTelemetry = new { variablesStartsFromES = 2 };
 
             var (_, resultTelemetry) = ProcessHandlerHelper.ExpandCmdEnv(argsLine);
 
-            Assert.Equal(expectedTelemetry.variablesStartsFromES, resultTelemetry.VariablesStartsFromES);
+            Assert.Equal(2, resultTelemetry.VariablesStartsFromES);
         }
     }
 }
