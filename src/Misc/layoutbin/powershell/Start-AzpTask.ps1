6235f97c4cf475f5ecc5ddea9c4bf5dac2baca4f
Tim Brigham <tbrigham@onecallcm.com>
2023-11-13 16:51:00 -0500

    Squashed commit of the following:
    commit b0d2f96cfc6d9d15aae190071260b2c605d812ff
    Author: Tim Brigham <tbrigham@onecallcm.com>
    Date:   Mon Nov 13 16:44:37 2023 -0500
    
        Updates to displayed notes
    
    commit 57c35991758ad5edc75c20d99f6e6dc4fde615c6
    Author: Tim Brigham <tbrigham@onecallcm.com>
    Date:   Mon Nov 13 16:19:32 2023 -0500
    
        Documentation for the pipeline if CLM is enabled.
    
    commit 08e7a390d49a074cf12ec0f4506c833082f5edef
    Author: Tim Brigham <tbrigham@onecallcm.com>
    Date:   Fri Nov 10 17:14:21 2023 -0500
    
        Description
    
    commit 6a4672ae7c45ad61fe866957d9e1c70d1a736684
    Author: Tim Brigham <tbrigham@onecallcm.com>
    Date:   Fri Nov 10 16:20:13 2023 -0500
    
        Improvements in verbose log mesages.
    
    (cherry picked from commit d92ceb8ddf6c0529c265bdd141cfe3af5dd6be3f)

--- src/Misc/layoutbin/powershell/Start-AzpTask.ps1
+++ src/Misc/layoutbin/powershell/Start-AzpTask.ps1
@@ -1,6 +1,6 @@
 <# 
 A PowerShell script that is used to invoke a VSTS task script. This script is used by the VSTS task runner to invoke the task script.
-This script resplaces some legacy stuff in PowerShell3Handler.cs and turns it into a dedicated function. 
+This script replaces some legacy stuff in PowerShell3Handler.cs and turns it into a dedicated signed script. 
 since it is parameterized it can be signed and trusted for WDAC and CLM.
 This is the original code that has been repalced - 
 @". ([scriptblock]::Create('if ([Console]::InputEncoding -is [Text.UTF8Encoding] -and [Console]::InputEncoding.GetPreamble().Length -ne 0) {{ [Console]::InputEncoding = New-Object Text.UTF8Encoding $false }} if (!$PSHOME) {{ $null = Get-Item -LiteralPath ''variable:PSHOME'' }} else {{ Import-Module -Name ([System.IO.Path]::Combine($PSHOME, ''Modules\Microsoft.PowerShell.Management\Microsoft.PowerShell.Management.psd1'')) ; Import-Module -Name ([System.IO.Path]::Combine($PSHOME, ''Modules\Microsoft.PowerShell.Utility\Microsoft.PowerShell.Utility.psd1'')) }}')) 2>&1 | ForEach-Object {{ Write-Verbose $_.Exception.Message -Verbose }} ; Import-Module -Name '{0}' -ArgumentList @{{ NonInteractive = $true }} -ErrorAction Stop ; $VerbosePreference = '{1}' ; $DebugPreference = '{1}' ; Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create('. ''{2}'''))",
@@ -34,10 +34,119 @@ $VerbosePreference = $DebugOption
 $DebugPreference = $DebugOption
 
 
-Write-Verbose "Pro Tip for CLM issues: xxx\_work\_tasks\xxx\xxx\powershell.ps1' is not recognized as the name of a cmdlet, function, script file, or operable program"
-Write-Verbose "This is a known issue with CLM files being loaded from a full language mode script. Be sure the listed file is whitelisted."
+function GetCLM {
+    # This is new functionality to detect if we are running in a constrained language mode.
+    # This is only used to display debug data if the device is in CLM mode by default.
+    # This test is modeled after the traditional __PSSCRIPTPOLICYTEST_LQU1DAME file
+    # This creates a temporary file with a changing signature 
+    # which will return the execution context language mode. 
+    $tempFileGuid = New-Guid | Select-Object -Expand Guid 
+    $tempFile = "$($env:temp)\$($tempFileGuid).ps1"
+    # First line is the command to get the language mode
+    Write-Output '$ExecutionContext.SessionState.LanguageMode' | Out-File -FilePath $tempFile 
+    Write-Output 'New-Object -TypeName System.Collections.ArrayList' | Out-File -FilePath $tempFile -append 
+    Write-Output "#  $tempFileGuid" | Out-File -FilePath $tempFile -append 
+    Write-Output "Write-Output '$tempFileGuid'" | Out-File -FilePath $tempFile -append 
+    # Now we execute the file and return the result. We need to try catch this because of how PS handles 
+    # the execution of CLM files in a non CLM environment.
+    try {
+        . $tempFile 
+        $status = "FullLanguage"
+    }
+    catch [System.Management.Automation.CommandNotFoundException] {
+        $status = "ConstrainedLanguage"
+    }
+    Remove-Item $tempFile 
+    return $status 
+}
+
+# This is the notes we want to display if CLM is active on the device by default.
+$verboseNotes = @"
+##############################
+## Basics ##
+Start-AzpTask is a pre handler function to safely hand off PowerShell commands. 
+It imports the module Invoke-VstsTaskScript and executes it with proper arguments.
+## Workflow ## 
+ADO Agent receives a request. The agent launches Agent.Worker.exe. 
+When the PowerShell3Handler within Agent.Worker.exe executes it opens a PowerShell instance, 
+launching Start-AzpTask script with arguments. Start-AzpTask receives a handler script file name to use
+(such as xxx\_tasks\CopyFiles_<guid>\xxx), along with the file name of an instance of 
+Invoke-VstsTaskScript for use (_tasks\xxx\xxx\ps_modules\VstsTaskSdk\xxx) 
+Debug display and error handling is also forwarded here.
+## Requirements ## 
+For pre handler script to work correctly on CLM enabled device, 
+full language mode must be available for itself, and everything it directly launches.
+Start-AzpTask (the output you are reading) language mode is: $($ExecutionContext.SessionState.LanguageMode). 
+If this isn't full language, fix this first.
+This instance is running from: $($MyInvocation.MyCommand.Path).
+Once Start-AzpTask is running in full language mode, verify that the 
+VstsTaskScript and handler are also in full language mode.
+Each script file used by Start-AzpTask must be signed or whitelisted to receive full language mode.
+The VstsTaskScript requested by this instance is: 
+$name
+The handler being requested by this instance is: 
+$scriptBlockString
+Please note other supporting files may also need to be signed or whitelisted to receive full language mode.
+These can be located using procmon, sysmon or similar tools.
+## Error Causes ##
+Start-AzpTask and the task handler scripts can run in different language modes. 
+Mixing language modes is not supported and will result in errors. 
+These may reflect as dot sourcing errors, or errors about a file not being recognized as 
+a cmdlet, function, script file, or operable program.
+## Advanced Usage ##
+Inline PowerShell from pipeline YML task definition is not signed. It runs in CLM by default.
+Signature blocks do not survive conversion from YML so inline scripts can't be signed.
+If you need full language mode for the task action being run by the handler, 
+you must execute it from disk, with proper signing or whitelisting in place.
+##############################
+
+"@
+
+# First we check if the device is in CLM mode by default.
+$clmResults = GetCLM 
+# Now the behavior based on CLM and debug settings.
+# Case 1, device is full language, continue as normal 
+if ( $clmResults -eq "FullLanguage" ) {
+    Write-Output "Full Language mode detected, continuing traditional workflow."
+    Write-Output ""
+}
+# Case 2, device is constrained language  
+else {
+    # 2a We have a request to display debug data and the device is in CLM
+    if ( $DebugOption -ceq "Continue" ) {
+        $verboseNotesString = $verboseNotes -split "\n" 
+        foreach ( $line in $verboseNotesString ) { 
+            Write-Verbose "$line" 
+        }
+    }
+    else {
+        # 2b We have a request to run in constrained language mode, but verbose is not set.
+        Write-Output "Constrained Language Mode detected, but verbose is not set. "
+        Write-Output "Suggest enabling system diagnostics if you need more information."
+    }
+    Write-Output ""
+}
 #Write-Verbose "ADO Pipeline Update Test Tim Brigham"
 
-Import-Module @importSplat 
-Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create( $scriptBlockString ))
+# Import the module and catch any errors
+try {
+    Import-Module @importSplat     
+}
+catch {
+    Write-Verbose $_.Exception.Message -Verbose 
+    throw $_.Exception
+}
+
+# Now create the task and hand of to the task script
+try {
+    Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create( $scriptBlockString ))
+}
+# We want to add improved error handling here - if the error is "xxx\powershell.ps1 is not recognized as the name of a cmdlet, function, script file, or operable program"
+# 
+catch {
+    Write-Verbose "Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create( $scriptBlockString ))"
+    Write-Verbose $_.Exception.Message -Verbose 
+    throw $_.Exception
+}
+#
 
