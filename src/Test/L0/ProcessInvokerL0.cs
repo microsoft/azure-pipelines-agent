5e17ea9e07850cbd95d82ee131c5f6f69b08529a
Ivan Duplenskikh <ivan.duplenskikh@akvelon.com>
2024-01-17 21:59:51 +0100

    Call CancelAfter

--- src/Test/L0/ProcessInvokerL0.cs
+++ src/Test/L0/ProcessInvokerL0.cs
@@ -59,15 +59,14 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests
                     Task execTask;
                     if (TestUtil.IsWindows())
                     {
-                        execTask = processInvoker.ExecuteAsync("", "cmd", $"/c \"choice /T {SecondsToRun} /D y\"", null, tokenSource.Token);
+                        execTask = processInvoker.ExecuteAsync("", "cmd", $"/c \"ping 127.0.0.1 -n {SecondsToRun} > nul\"", null, tokenSource.Token);
                     }
                     else
                     {
                         execTask = processInvoker.ExecuteAsync("", "bash", $"-c \"sleep {SecondsToRun}s\"", null, tokenSource.Token);
                     }
 
-                    await Task.Delay(14000);
-                    tokenSource.Cancel();
+                    tokenSource.CancelAfter(14000);
                     try
                     {
                         await execTask;
@@ -110,7 +109,6 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests
         [Trait("Level", "L0")]
         [Trait("Category", "Common")]
         [Trait("SkipOn", "darwin")]
-        [Trait("SkipOn", "windows")]
         public async Task TestCancelEnsureCompletedWhenTaskNotKilled()
         {
             const int SecondsToRun = 20;
