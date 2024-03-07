58ea8a5a4279510c3d72340937617eaa3e3ac739
Denis Nikulin (Akvelon Inc) <denis.nikulin@akvelon.com>
2024-03-07 14:57:37 +0400

    Added the information about the agent version to the timeline.

--- src/Agent.Worker/ExecutionContext.cs
+++ src/Agent.Worker/ExecutionContext.cs
@@ -781,6 +781,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
 
             var configuration = HostContext.GetService<IConfigurationStore>();
             _record.WorkerName = configuration.GetSettings().AgentName;
+            _record.Variables.Add(Constants.Variables.Agent.Version, BuildConstants.AgentPackage.Version);
 
             _jobServerQueue.QueueTimelineRecordUpdate(_mainTimelineId, _record);
         }
