de59d5dd2a3d1d73483e79034fed2133b3d5ff6f
Aditya Mankal <adityamankal@microsoft.com>
2024-10-24 17:30:51 +0530

    Fixed syntax error in dev.sh script

--- src/dev.sh
+++ src/dev.sh
@@ -120,6 +120,7 @@ function detect_platform_and_runtime_id() {
             DETECTED_RUNTIME_ID='win-x86'
         elif [[ "$PROCESSOR_ARCHITECTURE" == 'ARM64' ]]; then
             DETECTED_RUNTIME_ID='win-arm64'
+        if
     elif [[ "$CURRENT_PLATFORM" == 'linux' ]]; then
         DETECTED_RUNTIME_ID="linux-x64"
         if command -v uname >/dev/null; then
