ea3e6f54fa7dfa61563a1344e973304da93993d4
Sahil Manoj Bhatt <sahilbhatt@microsoft.com>
2025-01-06 14:16:45 +0530

    removing tracings - restoring few files to original state

--- src/Agent.Worker/TaskCommandExtension.cs
+++ src/Agent.Worker/TaskCommandExtension.cs
@@ -529,7 +529,6 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
                 !String.IsNullOrEmpty(doneText) &&
                 StringUtil.ConvertToBoolean(doneText))
             {
-                // Trace.Info("##DEBUG_SB: Calling ForceTaskComplete from TaskCompleteCommand");
                 context.ForceTaskComplete();
             }
         }
