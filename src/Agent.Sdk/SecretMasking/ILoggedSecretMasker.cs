52b4e2de0addf23dbf63edce4c46d40310cc76f5
Nick Guerrera <nguerrera@microsoft.com>
2025-04-21 17:06:24 -0500

    Send secret masking telemetry if opted in to it.
    If using the new OSS secret masker and an additional opt-in knob is turned on, then send telemetry about secrets that were masked.

--- src/Agent.Sdk/SecretMasking/ILoggedSecretMasker.cs
+++ src/Agent.Sdk/SecretMasking/ILoggedSecretMasker.cs
@@ -2,12 +2,19 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 
 namespace Agent.Sdk.SecretMasking
 {
     /// <summary>
-    /// Extended ISecretMasker interface that adds support for logging the origin of
-    /// regexes, encoders and literal secret values.
+    /// An action that publishes the given data corresonding to the given
+    /// feature to a telemetry channel.
+    /// </summary>
+    public delegate void PublishSecretMaskerTelemetryAction(string feature, Dictionary<string, string> data);
+
+    /// <summary>
+    /// Extended ISecretMasker interface that adds support for telemetry and
+    /// logging the origin of regexes, encoders and literal secret values.
     /// </summary>
     public interface ILoggedSecretMasker : IDisposable
     {
@@ -19,5 +26,8 @@ namespace Agent.Sdk.SecretMasking
         string MaskSecrets(string input);
         void RemoveShortSecretsFromDictionary();
         void SetTrace(ITraceWriter trace);
+
+        void StartTelemetry(int maxDetections);
+        void StopAndPublishTelemetry(int maxDetectionsPerEvent, PublishSecretMaskerTelemetryAction publishAction);
     }
 }
