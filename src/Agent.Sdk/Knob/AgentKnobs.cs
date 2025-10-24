160fcf645fa2e6beb612bbd62f47f35e919be30b
Surajit Shil <surajitshil@microsoft.com>
2025-10-24 10:23:38 +0530

    Fixing the order of the knob source for the EnableImmediateTimelineRecordUpdates

--- src/Agent.Sdk/Knob/AgentKnobs.cs
+++ src/Agent.Sdk/Knob/AgentKnobs.cs
@@ -631,9 +631,9 @@ namespace Agent.Sdk.Knob
         public static readonly Knob EnableImmediateTimelineRecordUpdates = new Knob(
             nameof(EnableImmediateTimelineRecordUpdates),
             "If true, timeline record updates will be sent immediately to the server instead of being queued",
+            new PipelineFeatureSource("EnableImmediateTimelineRecordUpdates"),
             new RuntimeKnobSource("AGENT_ENABLE_IMMEDIATE_TIMELINE_RECORD_UPDATES"),
             new EnvironmentKnobSource("AGENT_ENABLE_IMMEDIATE_TIMELINE_RECORD_UPDATES"),
-            new PipelineFeatureSource("EnableImmediateTimelineRecordUpdates"),
             new BuiltInDefaultKnobSource("false"));
 
         public static readonly Knob EnableResourceMonitorDebugOutput = new Knob(
