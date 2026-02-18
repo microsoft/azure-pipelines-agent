d3f745cce98c0735ba02821ac2e85a619c999aca
Surajit Shil <surajitshil@microsoft.com>
2026-02-18 14:50:53 +0530

    Adding logging statement

--- src/Agent.Sdk/Util/CertificateUtil.cs
+++ src/Agent.Sdk/Util/CertificateUtil.cs
@@ -18,20 +18,25 @@ namespace Agent.Sdk.Util
         public static X509Certificate2 LoadCertificate(string certificatePath, string password = null)
         {
 #if NET9_0_OR_GREATER
+            System.Diagnostics.Debug.WriteLine("CertificateUtil: Using NET9+ code path (X509CertificateLoader)");
             var contentType = X509Certificate2.GetCertContentType(certificatePath);
+            System.Diagnostics.Debug.WriteLine($"CertificateUtil: Certificate content type: {contentType}");
             switch (contentType)
             {
                 case X509ContentType.Cert:
                     // DER-encoded or PEM-encoded certificate
+                    System.Diagnostics.Debug.WriteLine("CertificateUtil: Loading as DER/PEM certificate using LoadCertificateFromFile");
                     return X509CertificateLoader.LoadCertificateFromFile(certificatePath);
 
                 case X509ContentType.Pkcs12:
                     // Note: X509ContentType.Pfx has the same value (3) as Pkcs12 refer: https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509contenttype?view=net-10.0
+                    System.Diagnostics.Debug.WriteLine("CertificateUtil: Loading as PFX/PKCS12 certificate using LoadPkcs12FromFile");
                     return X509CertificateLoader.LoadPkcs12FromFile(certificatePath, password);
 
                 default:
                     // For all other formats (Pkcs7, SerializedCert, SerializedStore, Authenticode, Unknown),
                     // use the legacy constructor with warning suppression
+                    System.Diagnostics.Debug.WriteLine($"CertificateUtil: Loading as {contentType} using legacy constructor");
 #pragma warning disable SYSLIB0057
                     if (string.IsNullOrEmpty(password))
                     {
@@ -43,9 +48,10 @@ namespace Agent.Sdk.Util
                     }
 #pragma warning restore SYSLIB0057
             }
-#else
+#else 
             // For .NET 8 and earlier, use the traditional constructor
             // The constructor automatically handles all certificate types
+            System.Diagnostics.Debug.WriteLine("CertificateUtil: Using legacy code path (X509Certificate2 constructor) for NET8 or earlier");
             if (string.IsNullOrEmpty(password))
             {
                 return new X509Certificate2(certificatePath);
