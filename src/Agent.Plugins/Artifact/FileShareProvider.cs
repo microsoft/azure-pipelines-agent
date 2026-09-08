90efb40b254e0c3965249f1cab333f62937710a7
dassayantan24 <dassayantan@microsoft.com>
2026-09-08 16:47:14 +0530

    Disableworker command for fileshareprovider

--- src/Agent.Plugins/Artifact/FileShareProvider.cs
+++ src/Agent.Plugins/Artifact/FileShareProvider.cs
@@ -149,7 +149,7 @@ namespace Agent.Plugins
             CancellationToken cancellationToken)
         {
             executionContext.Output(StringUtil.Loc("PublishingArtifactUsingRobocopy"));
-            using (var processInvoker = new ProcessInvoker(this.context))
+                        using (var processInvoker = new ProcessInvoker(this.context, disableWorkerCommands: true))
             {
                 // Save STDOUT from worker, worker will use STDOUT report unhandle exception.
                 processInvoker.OutputDataReceived += delegate (object sender, ProcessDataReceivedEventArgs stdout)
